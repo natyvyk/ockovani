066ab1b8eefd4eac20378dbd79f2e17eb9c7b6c3
Natalie Vykoukalova <naty.vykoukalova@gmail.com>
2024-10-14 14:40:10 +0200

    ockovani

---
+++ script.js
@@ -0,0 +1,4 @@
+const jmeno = prompt("Zadejte prosím vaše jméno a příjmení:");
+const vek = Number(prompt ("Zadejte prosím váš věk:"));
+
+document.body.innerHTML = "<p>" + jmeno + ", věk: " + vek + "</p>"
